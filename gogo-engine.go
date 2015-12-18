aaa0879f051049af052ffbd0a8e9fdc8e76c22b8
Kevin Hoffman <kehoffman@pivotal.io>
2015-12-18 10:28:56 -0500

    Splitting stuff up into functional categories.

--- gogo-engine.go
+++ gogo-engine.go
@@ -2,19 +2,6 @@ package gogo
 
 import "fmt"
 
-// PerformMove accepts an intent to perform a move, validates it, and returns
-// the corresponding change in match as a new match struct.
-func (gameboard GameBoard) performMove(move Move) (outBoard GameBoard, err error) {
-	//outBoard = NewBoard(cap(gameboard.Positions))
-	outBoard = gameboard.copy()
-
-	if gameboard.Positions[move.Position.X][move.Position.Y] != EmptyPosition {
-		return outBoard, fmt.Errorf(RulesFailureSpaceOccupied, move)
-	}
-	outBoard.Positions[move.Position.X][move.Position.Y] = move.Player
-	return outBoard, err
-}
-
 func (position Coordinate) String() string {
 	return fmt.Sprintf("(%d,%d)", position.X, position.Y)
 }
